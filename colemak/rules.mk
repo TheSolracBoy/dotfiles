e0e323dc018b3df17952d79a2f8ca6f09a3b7edc
charlieman2700 <carlos.ace27@gmail.com>
2024-02-28 10:54:47 -0600

    colemak layout config

---
+++ colemak/rules.mk
@@ -0,0 +1,16 @@
+# MOUSEKEY_ENABLE = yes    # Mouse keys
+# RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+# OLED_ENABLE     = yes
+# OLED_DRIVER     = SSD1306
+# LTO_ENABLE      = yes
+#
+#
+# TAP_DANCE_ENABLE = yes
+# OPT_ENABLE = yes
+#
+# SRC += callum.c
+SRC += oneshot.c
+SRC += swapper.c
+
+
+
