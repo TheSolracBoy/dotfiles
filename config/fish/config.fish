8368f797a54d1504e7c940e29fa482c46f45b60c
CharlieBytesX <carlos.ace27@gmail.com>
2024-06-05 13:25:58 -0600

    feat: Add linuxbrew to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,6 +10,7 @@ export CONFIG_DIR="$HOME/.config/lazygit"
 
 
 # Path
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 export PATH="$BUN_INSTALL/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
